c54bf93810e9de83e63ea0987a03c2f21e076639
benjaminwillls <bwills@deloitte.co.uk>
2023-02-16 12:12:31 +0000

    add docstrings and instructions

--- CDP_training/data_engineering/ETL_example_pandas.py
+++ CDP_training/data_engineering/ETL_example_pandas.py
@@ -11,6 +11,20 @@ parser.add_argument(
 
 
 def mkdir_if_not_exists(path: str) -> int:
+    """Will make a directory if it does not already exist in that filepath
+
+    Parameters
+    ----------
+    path : str
+        path to directory
+
+    Returns
+    -------
+    int
+        response code:
+            - 200 : successful
+            - 1 : unsuccessful
+    """
     if not os.path.exists(path):
         os.mkdir(path)
         return 200
@@ -18,13 +32,38 @@ def mkdir_if_not_exists(path: str) -> int:
 
 
 def drop_duplicates(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """Drops duplicates from a dataframe
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with no duplicates
+    """
     return dataframe.drop_duplicates()
 
 
 def save_data(
     dataframe: pd.DataFrame,
     loading_file_path: str,
-):
+) -> int:
+    """Saves data to the loading path specifed
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+    loading_file_path : str
+        path to have data be saved to
+
+    Returns
+    -------
+    int
+        response code:
+            - 200 : successful
+            - 1 : unsuccessful
+    """
     try:
         dataframe.to_csv(loading_file_path)
         return 200
@@ -43,6 +82,12 @@ def save_data(
 
 
 if __name__ == "__main__":
+    """
+    To use on cmd line:
+    python <path_to_python_file> --source_path <insert_source_path> --output_file_name <insert_desired_file_name>
+
+    This wills save the deduplicated file to the path ./landing/<file_name>, relative to whatever directory this is run from.
+    """
     args = parser.parse_args()
 
     SOURCE_FILE_PATH = args.source_path
