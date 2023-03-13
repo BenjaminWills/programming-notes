96a992d27d882397e1292663c2564513b6fd9528
benjaminwillls <bwills@deloitte.co.uk>
2023-03-13 15:41:16 +0000

    refactor

--- terraform/examples/local_example/local.tf
+++ terraform/examples/local_example/local.tf
@@ -2,6 +2,7 @@
 resource "local_file" "example"{
     filename = "./example.txt"
     content = path.cwd
+    file_permission = "0700"
 }
 
 # This file will create a file in the PWD that contains the path to the PWD.
