5c63364375310169f2cd1fabf6338f62f194e6f1
benjaminwillls <bwills@deloitte.co.uk>
2023-03-14 16:00:34 +0000

    add clarifiactoin

--- terraform/examples/datasource_example/main.tf
+++ terraform/examples/datasource_example/main.tf
@@ -3,6 +3,7 @@ resource "local_file" "new_file" {
   content  = data.local_file.local.content
 }
 
+# The data keyword implies that the file was NOT provisioned by terraform.
 data "local_file" "local" {
   filename = "./newfile.txt"
 }
