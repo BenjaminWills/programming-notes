b5d1e3bf334d0e14b330da8fbf9d9031d5e64442
benjaminwillls <bwills@deloitte.co.uk>
2023-03-14 09:59:03 +0000

    add variables

--- terraform/examples/local_example/main.tf
+++ terraform/examples/local_example/main.tf
@@ -1,13 +1,13 @@
 # block name, [provider]_[resource type], resource name
 resource "local_file" "example"{
-    filename = "./example.txt"
+    filename = var.example["filename"]
     content = path.cwd
-    file_permission = "0700"
+    file_permission = var.example["file_permission"]
 }
 
 resource "random_pet" "random-example" {
-  prefix = "Mr"
-  separator = "."
-  length = "1"
+  prefix = var.random_example["prefix"]
+  separator = var.random_example["separator"]
+  length = var.random_example["length"]
 }
 # This file will create a file in the PWD that contains the path to the PWD.
---
+++ terraform/examples/local_example/variables.tf
@@ -0,0 +1,16 @@
+variable "example" {
+  type = map(string)
+  default = {
+    "filename" = "./example.txt"
+    "file_permission" = "0700"
+  }
+}
+
+variable "random_example" {
+  type = map(string)
+  default = {
+    "prefix" = "Mr"
+    "separator" = "."
+    "length" = "1"
+  }
+}
